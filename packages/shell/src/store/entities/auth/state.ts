6e7d45b9896ecec87db64bd3a7c62ecbd5798591
MikeT <michail.tsougkranis@gmail.com>
2021-01-08 18:33:36 +0200

    initial state for img

--- packages/shell/src/store/entities/auth/state.ts
+++ packages/shell/src/store/entities/auth/state.ts
@@ -2,5 +2,5 @@ import { AuthReducerType } from "./types";
 export const initialState: AuthReducerType = {
   isLoading: false,
   error: null,
-  data: { description: "", displayName: "", link: "" },
+  data: { description: "", displayName: "", link: "", img: "" },
 };
